5781b12764f4c555c82d2baf1cc85ca965acab1e
bunty sharma <bantysharma2614@gmail.com>
2026-09-08 23:26:18 +0530

    one code

---
+++ Ifelse/Example09.java
@@ -0,0 +1,32 @@
+package Ifelse;
+
+import java.util.Scanner;
+
+public class Example09 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter day number: ");
+        int day = sc.nextInt();
+
+        if (day == 1) {
+            System.out.println("Monday");
+        } else if (day == 2) {
+            System.out.println("Tuesday");
+        } else if (day == 3) {
+            System.out.println("Wednesday");
+        } else if (day == 4) {
+            System.out.println("Thursday");
+        } else if (day == 5) {
+            System.out.println("Friday");
+        } else if (day == 6 || day == 7) {
+            System.out.println("Weekend");
+        } else {
+            System.out.println("Invalid day number");
+        }
+    }
+}
+
+
+
