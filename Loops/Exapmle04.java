423418e730a95d5f9cc1c5fda9931c1b9c6aa564
bunty sharma <bantysharma2614@gmail.com>
2026-09-24 23:29:56 +0530

    one code

---
+++ Loops/Exapmle04.java
@@ -0,0 +1,24 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class Exapmle04 {
+    public static void main(String[] args) {
+
+            Scanner sc = new Scanner(System.in);
+            System.out.println("enter unit");
+            int units = sc.nextInt();
+            int bill;
+
+            if (units < 100) {
+                bill = units * 5;
+            } else if (units <= 200) {
+                bill = (100 * 5) + ((units - 100) * 7);
+            } else {
+                bill = (100 * 5) + (100 * 7) + ((units - 200) * 10);
+            }
+
+            System.out.println("total bill=" +  bill);
+        }
+    }
+
